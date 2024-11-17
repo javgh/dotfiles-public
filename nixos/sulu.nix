e56b289c3e5e44d0147be4edf8b51dd034d64f93
Jan Vornberger <jan@271.de>
2024-11-17 18:29:53 +0100

    Configure RAID and disk encryption for laptop

--- nixos/sulu.nix
+++ nixos/sulu.nix
@@ -1,3 +1,10 @@
+# Use cfdisk to prepare a GUID partition table (GPT) on the disk:
+#   /dev/nvme0n1p1       2048     616447     614400   300M EFI-System
+#   /dev/nvme0n1p2     616448 1814652927 1814036480   865G Linux RAID
+#   /dev/nvme0n1p3 1814652928 3628689407 1814036480   865G Linux RAID
+#   /dev/nvme0n1p4 3628689408 3907028991  278339584 132,7G Linux Swap
+# See doc/raid+integrity for RAID details.
+# Use cryptsetup {luksFormat,luksDump} to configure and check encryption.
 { pkgs, lib, ... }:
 
 {
@@ -16,8 +23,8 @@
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/7331a140-5df1-4c9c-b4b0-68ef45db89d8"; }
-  ];
+    { device = "/dev/disk/by-uuid/af0a91b3-40a5-42fb-a7cb-404e2cfb0f5e"; options = [ "nofail" ]; }
+  ];  # swap should not be a hard requirement for system startup
 
   boot = {
     initrd = {
@@ -29,13 +36,58 @@
         "xhci_pci"
       ] ++ [
         "dm_integrity"
+        "dm_crypt"
+        "dm_mod"
+        "cbc"
+        "hmac"
+        "sha256"
+        "rng"
+        "aes"
+        "encrypted_keys"
+        "trusted"
       ];
 
       extraUtilsCommands = ''
         copy_bin_and_libs ${pkgs.cryptsetup}/bin/integritysetup
+        copy_bin_and_libs ${pkgs.cryptsetup}/bin/cryptsetup
       '';
       postDeviceCommands = ''
         integritysetup open /dev/nvme0n1p2 nvme0n1p2+integrity
+        integritysetup open /dev/nvme0n1p3 nvme0n1p3+integrity
+        mdadm --stop --scan  # start with a clean slate
+        mdadm --assemble --scan --run
+
+        while true; do
+            read -s -p "Ready to decrypt disk. Please provide passphrase: " passphrase
+            echo
+            echo -n "$passphrase" | cryptsetup open /dev/md/mirrored mirrored+decrypted
+
+            if [ $? == 0 ]; then
+                break
+            fi
+        done
+
+        while true; do
+            echo -n "$passphrase" | cryptsetup open /dev/nvme0n1p4 nvme0n1p4+decrypted
+
+            if [ $? == 0 ]; then
+                break
+            else
+                read -s -p "Ready to decrypt swap. Please provide passphrase: " passphrase
+                echo
+            fi
+        done
+
+        passphrase=""
+      '';
+    };
+
+    swraid = {
+      enable = true;
+      mdadmConf = ''
+        PROGRAM /bin/true
+        DEVICE /dev/mapper/nvme0n1p2+integrity
+        DEVICE /dev/mapper/nvme0n1p3+integrity
       '';
     };
 
